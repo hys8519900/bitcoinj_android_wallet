4884f081b13ed686e41857aaef1fe690a4a72d33
unknown <123@.(none)>
2014-10-12 10:42:03 +0800

    add uses-permission

--- app/src/main/java/test/example/wg/testbitcoinj/BitcoinjService.java
+++ app/src/main/java/test/example/wg/testbitcoinj/BitcoinjService.java
@@ -2,9 +2,14 @@ package test.example.wg.testbitcoinj;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Environment;
 import android.os.IBinder;
 import android.util.Log;
 import com.google.bitcoin.core.*;
+import com.google.bitcoin.kits.WalletAppKit;
+import com.google.bitcoin.params.TestNet3Params;
+
+import java.io.File;
 import java.util.Date;
 
 public class BitcoinjService extends Service {
@@ -23,11 +28,15 @@ public class BitcoinjService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.v("service", "onStartCommand()" + startId + ": " + intent);
+        Log.v("service", "onStartCommand() id:" + startId + ": " + intent);
 
         new Thread(new Runnable() {
             @Override
             public void run() {
+                //NetworkParameters params = TestNet3Params.get();
+                //WalletAppKit kit = new WalletAppKit(params, new File(Environment.getExternalStorageDirectory().toString()), "walletappkit-example");
+                //kit.startAndWait();
+                Log.v("Sdcard ",Environment.getExternalStorageDirectory().toString());
 
             }
         }).start();
